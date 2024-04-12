1c607dbc038a6dbf030964dd99c6d29f8370e182
Josue Vega <j.vegav@uniandes.edu.co>
2024-04-12 06:53:05 -0500

    changes in authentication twilio

--- banco/banco/logic/callAPI.py
+++ banco/banco/logic/callAPI.py
@@ -6,21 +6,21 @@ class ManageApi:
 
     phone =''
     
-    def send_message(self, phonenumber):  # Ensure self parameter for instance method
+    def send_message( phonenumber):  # Ensure self parameter for instance method
         account_sid = 'AC75aecc0653315a05c12121973623ef5e'
-        auth_token = '63a01eb1b86b82ca3a43189fdb8bd396'
+        auth_token = 'b8e4beaf8e7e3ec8919bcf9a724518a7'
         client = Client(account_sid, auth_token)
         ManageApi.phone = phonenumber
 
-        verification = client.verify.v2.services('VA5cd775f75902274c8e28542444bfb7b0').verifications.create(
+        verification = client.verify.v2.services('VA3935003e5710342a24b804ea84c7b5f7').verifications.create(
             to=phonenumber, channel="sms"
         )
 
     def verify_otp( phonenumber, otp):  # Ensure self parameter for instance method
         account_sid = 'AC75aecc0653315a05c12121973623ef5e'
-        auth_token = '63a01eb1b86b82ca3a43189fdb8bd396'
+        auth_token = 'b8e4beaf8e7e3ec8919bcf9a724518a7'
         client = Client(account_sid, auth_token)
-        otp_check = client.verify.v2.services('VA5cd775f75902274c8e28542444bfb7b0').verification_checks.create(
+        otp_check = client.verify.v2.services('VA3935003e5710342a24b804ea84c7b5f7').verification_checks.create(
             to=phonenumber, code=otp  # Use self to access instance attribute
         )
         if otp_check.status.lower() == 'approved':
