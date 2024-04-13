3a21af3ccb2df09b403999ac3de511f435d41376
Josue Vega <j.vegav@uniandes.edu.co>
2024-04-12 19:08:37 -0500

    new service and the experiment

--- banco/banco/logic/callAPI.py
+++ banco/banco/logic/callAPI.py
@@ -7,20 +7,20 @@ class ManageApi:
     phone =''
     
     def send_message( phonenumber):  # Ensure self parameter for instance method
-        account_sid = 'ACbdf30ee3a2080be25b4501d736941cea'
-        auth_token = 'ae8136b74199309631d7564bd1d589b0'
+        account_sid = 'AC1ecafd15aa3fc0a2974c61a972f1453f'
+        auth_token = 'dd85c804f8e5c1b59df294c82f823c48'
         client = Client(account_sid, auth_token)
         ManageApi.phone = phonenumber
 
-        verification = client.verify.v2.services('VA3ebb825e5495dcab90a077038c7ecf9f').verifications.create(
+        verification = client.verify.v2.services('VA30a60815e20f6680b5056162b2fc1926').verifications.create(
             to=phonenumber, channel="sms"
         )
 
     def verify_otp( phonenumber, otp):  # Ensure self parameter for instance method
-        account_sid = 'ACbdf30ee3a2080be25b4501d736941cea'
-        auth_token = 'ae8136b74199309631d7564bd1d589b0'
+        account_sid = 'AC1ecafd15aa3fc0a2974c61a972f1453f'
+        auth_token = 'dd85c804f8e5c1b59df294c82f823c48'
         client = Client(account_sid, auth_token)
-        otp_check = client.verify.v2.services('VA3ebb825e5495dcab90a077038c7ecf9f').verification_checks.create(
+        otp_check = client.verify.v2.services('VA30a60815e20f6680b5056162b2fc1926').verification_checks.create(
             to=phonenumber, code=otp  # Use self to access instance attribute
         )
         if otp_check.status.lower() == 'approved':
